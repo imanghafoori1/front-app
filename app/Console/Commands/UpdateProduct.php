f8d7351970ea7df1c728b64fa1a572fd5a57bb8f
Iman <imanghafoori1@gmail.com>
2025-04-03 19:31:57 +0330

    remove useless constructor

--- app/Console/Commands/UpdateProduct.php
+++ app/Console/Commands/UpdateProduct.php
@@ -24,16 +24,6 @@ class UpdateProduct extends Command
     protected $description = 'Update a product with the specified details';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return int
